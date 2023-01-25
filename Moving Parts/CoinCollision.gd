0fb36123023ea84e8268131e5300820debab1c6b
Bebop <nicolas.bibboperez@gmail.com>
2023-01-24 22:15:22 -0300

    Coin flip animations

--- Moving Parts/CoinCollision.gd
+++ Moving Parts/CoinCollision.gd
@@ -2,20 +2,27 @@ extends Area2D
 
 const IMPULSE = Vector2(65, 0)
 
-signal coins_combined
+signal combine(value)
 
 
 func _on_CoinCollision_body_entered(_body):
 	var coin = get_parent()
 	if !coin.is_moving:
-		if coin.orientation == "LEFT":
-			coin.apply_central_impulse(IMPULSE)
-		elif coin.orientation == "RIGHT":
-			coin.apply_central_impulse(-IMPULSE)
+		push_other(coin)
 
 	var bodies = get_overlapping_bodies()
 	if bodies.size() > 1:
-		if bodies[0].is_moving && bodies[1].is_moving:
-			print("2 coins")
-			bodies[1].queue_free()
-			emit_signal("coins_combined")
+		combine(bodies)
+
+
+func push_other(coin):
+	if coin.orientation == "LEFT":
+		coin.apply_central_impulse(IMPULSE)
+	elif coin.orientation == "RIGHT":
+		coin.apply_central_impulse(-IMPULSE)
+
+
+func combine(coins):
+	if coins[0].is_moving && coins[1].is_moving:
+		coins[0].queue_free()
+		emit_signal("combine", coins[0].value + coins[1].value)
