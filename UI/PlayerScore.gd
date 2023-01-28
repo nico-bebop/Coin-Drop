16285134237631d383e91fd02346601a989f02ff
Bebop <nicolas.bibboperez@gmail.com>
2023-01-28 01:49:42 -0300

    Removed scene complexity

--- UI/PlayerScore.gd
+++ UI/PlayerScore.gd
@@ -9,7 +9,7 @@ func _ready():
 	text = player_name + "\nSCORE: " + str(score)
 
 
-func _on_BoardBottom_update_score(player, value):
+func update_score(player, value):
 	if text.begins_with(player):
 		score += value
 		text = player_name + "\nSCORE: " + str(score)
