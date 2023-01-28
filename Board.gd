7381aec3e7fb7bc060d4fdeaa8a56f6a66b57dcf
Bebop <nicolas.bibboperez@gmail.com>
2023-01-28 03:46:16 -0300

    Initial rounds system

--- Board.gd
+++ Board.gd
@@ -1,13 +1,23 @@
 extends Node2D
 
-var current_turn setget set_turn
 var active_coins
+var current_turn setget set_turn
+var current_round = 0 setget set_round
+var required_score
+
+enum scores {
+	ROUND_ONE = 10,
+	ROUND_TWO = 40,
+	ROUND_THREE = 20,
+	ROUND_FOUR = 80,
+}
 
 onready var player1 = $UserInterface/PlayerScore1
 onready var player2 = $UserInterface/PlayerScore2
 onready var slots = $Slots
 
 signal turn_ready
+signal round_ended(next_round)
 
 
 func _ready():
@@ -18,7 +28,7 @@ func _ready():
 	else:
 		set_turn(player2.player_name)
 		change_outlines(player2, player1)
-
+	set_required_score()
 
 func next_turn():
 	match current_turn:
@@ -29,14 +39,37 @@ func next_turn():
 			set_turn(player1.player_name)
 			change_outlines(player1, player2)
 	emit_signal("turn_ready")
+	check_change_round()
 
 
 func set_turn(player):
 	current_turn = player
 
 
+func set_round(current):
+	current_round = current
+	set_required_score()
+
+
+func set_required_score():
+	match current_round:
+		0:
+			required_score = scores.ROUND_ONE
+		1:
+			required_score = scores.ROUND_TWO
+		2:
+			required_score = scores.ROUND_THREE
+		3:
+			required_score = scores.ROUND_FOUR
+
+
+func check_change_round():
+	if player1.score >= required_score || player2.score >= required_score:
+		set_round(current_round + 1)
+		emit_signal("round_ended", current_round)
+
+
 func change_outlines(active_player, inactive_player):
-	set_turn(active_player.player_name)
 	active_player.get("custom_fonts/font").outline_size = 1
 	inactive_player.get("custom_fonts/font").outline_size = 0
 
--- Fixed Parts/BoardBottom.gd
+++ Fixed Parts/BoardBottom.gd
@@ -1,7 +1,10 @@
 extends Node2D
 
-signal update_score(player, value)
 
-func _on_ScoreSlot_add_score(value):
-	var player = get_tree().current_scene.current_turn
-	emit_signal("update_score", player, value)
+func _on_Board_round_ended(next_round):
+	change_slots_scores(next_round)
+
+
+func change_slots_scores(next):
+	for slot in get_children():
+		slot.set_score(slot.score_array[next])
