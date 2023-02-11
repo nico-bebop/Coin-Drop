89721e1d6a8bef4066e9c47a936f4a1b42e292b2
Bebop <nicolas.bibboperez@gmail.com>
2023-02-11 02:11:46 -0300

    Bomb basic animation with spark

--- board/moving_parts/bomb.gd
+++ board/moving_parts/bomb.gd
@@ -1,20 +1,53 @@
 extends "res://board/moving_parts/ball.gd"
 
-export(int) var ticks_left = 4
+const SPARK_POSITIONS = [Vector2(8, -6), Vector2(5, -6), Vector2(2, -8)]
 
+export(int) var ticks_left = 3
+
+var ignited = false
+
+onready var animated_sprite = $AnimatedSprite
 onready var label = $Label
+onready var spark = $Spark
 onready var switch_collision = $SwitchCollision
 
 
-func tick():
-	ticks_left -= 1
+func _ready():
 	update_label()
+
+
+func tick():
+	if ignited:
+		ticks_left -= 1
+		update_label()
+		update_animation()
+	else:
+		ignite()
+
 	if ticks_left == 0:
 		explode()
 
 
 func update_label():
 	label.text = str(ticks_left)
+
+
+func update_animation():
+	match ticks_left:
+		3:
+			animated_sprite.frame = 0
+			spark.position = SPARK_POSITIONS[0]
+		2:
+			animated_sprite.frame = 1
+			spark.position = SPARK_POSITIONS[1]
+		1:
+			animated_sprite.play("RedGlow")
+			spark.position = SPARK_POSITIONS[2]
+
+
+func ignite():
+	ignited = true
+	spark.emitting = true
 
 
 func explode():
