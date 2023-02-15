56dabb8f1fa4b9d11cc11c91e7a3c7917d3d15b3
Bebop <nicolas.bibboperez@gmail.com>
2023-02-14 21:13:01 -0300

    Menu buttons sounds

--- board/ui/user_interface.gd
+++ board/ui/user_interface.gd
@@ -18,6 +18,10 @@ onready var accept_button = $Buttons/AcceptButton
 onready var cancel_button = $Buttons/CancelButton
 onready var quit_button = $Buttons/QuitButton
 
+onready var button_click_audio = $Buttons/ButtonClickAudio
+onready var pause_audio = $Buttons/PauseAudio
+onready var unpause_audio = $Buttons/UnPauseAudio
+
 
 func _ready():
 	animation_player.play("SignAppear")
@@ -26,24 +30,30 @@ func _ready():
 
 func _on_PauseButton_pressed():
 	if !get_tree().paused:
+		pause_audio.play()
 		pause(true, PAUSED, ResumeButton)
 	else:
+		unpause_audio.play()
 		pause(false, TITLE, PauseButton)
 
 
 func _on_RestartButton_pressed():
+	button_click_audio.play()
 	restart(true, RESTART)
 
 
 func _on_CancelButton_pressed():
+	button_click_audio.play()
 	restart(false, TITLE)
 
 
 func _on_AcceptButton_pressed():
+	button_click_audio.play()
 	SceneTransition.restart_scene()
 
 
 func _on_QuitButton_pressed():
+	button_click_audio.play()
 	get_tree().paused = false
 	SceneTransition.change_scene("res://menu/main_menu.tscn")
 
