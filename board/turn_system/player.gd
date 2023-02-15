8f66d1c7a6f1c4d61b68ba1e6c287f36d31ae992
Bebop <nicolas.bibboperez@gmail.com>
2023-02-15 14:05:49 -0300

    Single player objective

--- board/turn_system/player.gd
+++ board/turn_system/player.gd
@@ -11,6 +11,7 @@ var coins_left = coins_per_round
 var total_score = 0
 var round_score = 0
 var required_score = [10, 40, 20, 80, Globals.NO_SCORE]
+var objective
 
 onready var animation_player = $AnimationPlayer
 onready var coin_meter = $CoinMeter
@@ -22,6 +23,7 @@ onready var tween = $Tween
 func _ready():
 	$Scoreboard/PlayerName.text = player_name
 	animation_player.play("Appear")
+
 
 func reset_scoreboard():
 	empty_coin_meter()
@@ -42,6 +44,7 @@ func set_label_text():
 	match Globals.game_mode:
 		Globals.GameModes.SINGLE_PLAYER:
 			score.text = COINS_LEFT + str(coins_left)
+			turn_system.set_objective_text()
 		Globals.GameModes.VERSUS:
 			score.text = ROUND_SCORE + str(round_score) + "/" + str(required_score[turn_system.current_round])
 
--- board/turn_system/single_player.gd
+++ board/turn_system/single_player.gd
@@ -1,12 +1,16 @@
 extends "res://board/turn_system/turn_system.gd"
 
 const GAME_OVER = ["Game", "Over!"]
+const GET = "GET\n"
+const POINTS_THIS_ROUND = " POINTS\nTHIS ROUND\n"
 
 onready var player = $Player
-onready var decoration = $Decoration/AnimationPlayer
+onready var objective = $Player/Objective/Scoreboard/Score
+onready var objective_animation = $Player/Objective/AnimationPlayer
+
 
 func _ready():
-	decoration.play("Appear")
+	objective_animation.play("Appear")
 
 
 func start():
@@ -25,6 +29,15 @@ func should_change_round():
 func start_round():
 	player.coins_left = player.coins_per_round
 	player.reset_scoreboard()
+
+
+func set_objective_text():
+	if current_round == Globals.FINAL_ROUND:
+		objective.text = ""
+		return
+
+	objective.text = GET + str(player.required_score[current_round]) + POINTS_THIS_ROUND
+	objective.text += str(player.round_score) + "/" + str(player.required_score[current_round])
 
 
 func game_over():
