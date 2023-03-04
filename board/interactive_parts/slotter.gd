5d2df0a723e33914ea272988a9c14aaf3d75cb98
Bebop <nicolas.bibboperez@gmail.com>
2023-03-04 02:48:40 -0300

    Slotter polish

--- board/interactive_parts/slotter.gd
+++ board/interactive_parts/slotter.gd
@@ -5,9 +5,18 @@ const STEP = 20
 
 onready var animation_player = $AnimationPlayer
 onready var coin_sprite = $CoinSprite
+onready var tween = $Tween
 onready var balls = $"../Balls"
 
 signal subtract_coin
+
+
+func _ready():
+	tween.interpolate_property($Slider, "value", 1, 8, 1.5, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, 0.5)
+	tween.start()
+	yield(tween, "tween_completed")
+	tween.interpolate_property($Slider, "value", 8, 1, 1.5)
+	tween.start()
 
 
 func _on_Slider_value_changed(value):
@@ -15,10 +24,11 @@ func _on_Slider_value_changed(value):
 	
 
 func _on_Slider_drag_ended(_value_changed):
-	emit_signal("subtract_coin")
-	yield(coin_sprite, "animation_finished")
-	disable_slotter()
-	balls.spawn_coin(coin_sprite.global_position)
+	if coin_sprite.visible:
+		emit_signal("subtract_coin")
+		yield(coin_sprite, "animation_finished")
+		disable_slotter()
+		balls.spawn_coin(coin_sprite.global_position)
 
 
 func _on_TurnSystem_turn_ready():
@@ -26,19 +36,15 @@ func _on_TurnSystem_turn_ready():
 
 
 func _on_TurnSystem_game_over(_message, _first_loss):
+	$Slider.editable = false
 	disable_slotter()
 
 
 func enable_slotter():
-	change_slotter_state(true)
+	coin_sprite.visible = true
 	animation_player.play("Highlight")
 
 
 func disable_slotter():
-	change_slotter_state(false)
+	coin_sprite.visible = false
 	animation_player.play("RESET")
-
-
-func change_slotter_state(value):
-	$Slider.editable = value
-	coin_sprite.visible = value
