c6028410498f339aa476b1f10e28162ce93c4969
Bebop <nicolas.bibboperez@gmail.com>
2023-03-06 16:58:07 -0300

    Floating text on power up pickup

--- board/power_ups/extra_coins.gd
+++ board/power_ups/extra_coins.gd
@@ -5,10 +5,15 @@ const EXTRA_COINS = 3
 onready var player = $"../../TurnSystem/SinglePlayer/Player"
 
 
+func _ready():
+	pickup_text = str(EXTRA_COINS) + pickup_text
+	
+
 func _on_BallCollision_body_entered(body):
 	disappear()
 
 	if body.is_in_group(Globals.GROUP_COINS):
+		pick_up()
 		player.coins_left += EXTRA_COINS
 		player.set_label_text()
 		$PowerUpAudio.play()
--- board/power_ups/power_up.gd
+++ board/power_ups/power_up.gd
@@ -1,6 +1,9 @@
 extends RigidBody2D
 
+export(String) var pickup_text
+
 onready var animation_player = $AnimationPlayer
+onready var effects = $"../../AfterFreeEffects"
 
 
 func _ready():
@@ -14,3 +17,7 @@ func disappear():
 	$CollisionShape2D.queue_free()
 	$Sprite.visible = false
 	$Sparkle.emitting = false
+
+
+func pick_up():
+	effects.instance_floating_text(pickup_text, global_position)
