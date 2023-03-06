41df11c68aef3119c27decbedb330f90ffe8fc49
Bebop <nicolas.bibboperez@gmail.com>
2023-03-06 01:58:28 -0300

    Google Play Services less invasive

--- integrations/google_play.gd
+++ integrations/google_play.gd
@@ -3,27 +3,44 @@ extends Node
 const CLIENT_ID = "311116801314-jeauebh6glo66fv8eedbnes5d0tj7hat.apps.googleusercontent.com"
 const LEADERBOARD_ID = "CgkIosqGgIcJEAIQAA"
 
-var play_games_services
+var service
 
 
 func _ready():
 	if Engine.has_singleton("GodotPlayGamesServices"):
-		play_games_services = Engine.get_singleton("GodotPlayGamesServices")
-		play_games_services.init(true, true, true, "")
-	if play_games_services:
+		service = Engine.get_singleton("GodotPlayGamesServices")
+		service.init(true, true, true, "")
+	if service:
 		connect_signals()
-		sign_in()
 
 
 func sign_in():
-	play_games_services.signIn()
+	service.signIn()
+
+
+func submit_score(score):
+	if service && service.isSignedIn():
+		service.submitLeaderBoardScore(LEADERBOARD_ID, score)
+		Globals.offline_score = 0
+	else:
+		Globals.offline_score = score
+
+
+func show_leaderboard():
+	if service:
+		if !service.isSignedIn():
+			sign_in()
+		else:
+			if Globals.offline_score != 0:
+				submit_score(Globals.offline_score)
+			service.showLeaderBoard(LEADERBOARD_ID)
 
 
 func connect_signals():
-	play_games_services.connect("_on_sign_in_success", self, "_on_sign_in_success")  # account_id: String
-	play_games_services.connect("_on_sign_in_failed", self, "_on_sign_in_failed")  # error_code: int
-	play_games_services.connect("_on_leaderboard_score_submitted", self, "_on_leaderboard_score_submitted")  # leaderboard_id: String
-	play_games_services.connect("_on_leaderboard_score_submitting_failed", self, "_on_leaderboard_score_submitting_failed")  # leaderboard_id: String
+	service.connect("_on_sign_in_success", self, "_on_sign_in_success")  # account_id: String
+	service.connect("_on_sign_in_failed", self, "_on_sign_in_failed")  # error_code: int
+	service.connect("_on_leaderboard_score_submitted", self, "_on_leaderboard_score_submitted")  # leaderboard_id: String
+	service.connect("_on_leaderboard_score_submitting_failed", self, "_on_leaderboard_score_submitting_failed")  # leaderboard_id: String
 
 
 func _on_sign_in_success(account_id):
@@ -40,13 +57,3 @@ func _on_leaderboard_score_submitted(leaderboard_id):
 
 func _on_leaderboard_score_submitting_failed(leaderboard_id):
 	print("Score submitting failed - ", leaderboard_id)
-
-
-func submit_score(score):
-	if play_games_services:
-		play_games_services.submitLeaderBoardScore(LEADERBOARD_ID, score)
-
-
-func show_leaderboard():
-	if play_games_services:
-		play_games_services.showLeaderBoard(LEADERBOARD_ID)
